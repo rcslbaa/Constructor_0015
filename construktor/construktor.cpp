84ce11bfb3bac12ba4ef2824455041903bd8b410
rcslbaa <ifasmarani3125@gmail.com>
2024-05-14 18:55:02 +0700

    menambahkan class mahasiswa public dan void cetak

--- construktor/construktor.cpp
+++ construktor/construktor.cpp
@@ -7,3 +7,30 @@ class Mahasiswa
 public:
 	int nim;
 	string nama;
+
+public:
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = "";
+	};
+	Mahasiswa(int iNim)
+	{
+		nim = iNim;
+
+	};
+	Mahasiswa(string iNama)
+	{
+		nama = iNama;
+	};
+	Mahasiswa(int iNim, string iNama)
+	{
+		nim = iNim;
+		nama = iNama;
+	};
+	void cetak() {
+		cout << "NIM = " << nim << endl;
+		cout << "Nama = " << nama << endl;
+		cout << endl;
+	};
+};

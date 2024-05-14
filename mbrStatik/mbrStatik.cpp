0bddd810d4ca0b3aa6bbdca6a5d50043302a0a03
rcslbaa <ifasmarani3125@gmail.com>
2024-05-14 17:50:00 +0700

    menambahkan class mahasiswa

--- mbrStatik/mbrStatik.cpp
+++ mbrStatik/mbrStatik.cpp
@@ -18,33 +18,3 @@ public:
 		setID();
 	}
 };
-
-int mahasiswa::nim = 113;
-
-void mahasiswa::setID()
-{
-	id = nim++;
-}
-
-void mahasiswa::printAll()
-{
-	cout << "ID   =" << id << endl;
-	cout << "Nama =" << nama << endl;
-	cout << endl;
-}
-
-int main()
-{
-	mahasiswa mhs1("Lia Kurnia");
-	mahasiswa mhs2("Asroni");
-	mahasiswa mhs3("Andi Kurniawan");
-	mahasiswa mhs4("Joko Purbo");
-
-
-	mhs1.printAll();
-
-	mhs2.printAll();
-	mhs3.printAll();
-	mhs4.printAll();
-	return 0;
-}
